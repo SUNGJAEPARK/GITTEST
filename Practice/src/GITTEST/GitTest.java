e70ee7ccd89c85134d1278fccbae575fb8a2a13f
SUNJGJAEPARK <sj.park@isecommerce.co.kr>
2015-03-10 16:22:42 +0900

    11111

--- Practice/src/GITTEST/GitTest.java
+++ Practice/src/GITTEST/GitTest.java
@@ -5,7 +5,6 @@ public class GitTest {
 	public static void main(String[] args) {
 
 		System.out.println("break_testBranch");
-		System.out.println("A PC에서 작업함");
 		
 		System.out.println("B PC에서 작업함");
 	}
