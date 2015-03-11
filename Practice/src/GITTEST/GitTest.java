e2ea77a1b45c030eb4ec6a3cd139aded0165ae30
park <park@PC.localdomain>
2015-03-11 14:48:04 +0900

    1번 개발차가 1차 수정한것을 머지하였음

--- Practice/src/GITTEST/GitTest.java
+++ Practice/src/GITTEST/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("1번 개발자가 수정함");
+		System.out.println("1번 개발자가 1차 수정함");
 
 		System.out.println("2번 개발자가 1차 수정함");
 	}
